f7fa4fd5c84ccf6bf2b3dfb098ca17f4277af7da
Oscar Torres <oscartorres@Oscars-MacBook-Pro.local>
2017-09-28 13:58:35 -0500

    Init commit

---
+++ src/com/bittle/urban/Definition.java
@@ -0,0 +1,76 @@
+package com.bittle.urban;
+
+import com.google.gson.JsonObject;
+
+public class Definition {
+    private String def = "";
+    private String permaLink = "";
+    private long thumbsUp = 0;
+    private String author = "";
+    private String word = "";
+    private long defid = 0;
+    private String currentVote = "";
+    private String example = "";
+    private long thumbsDown = 0;
+
+    public Definition(JsonObject obj){
+        def = obj.get("definition").getAsString();
+        permaLink = obj.get("permalink").getAsString();
+        thumbsUp =  strToL(obj.get("thumbs_up").getAsString());
+        author = obj.get("author").getAsString();
+        word = obj.get("word").getAsString();
+        defid = strToL(obj.get("defid").getAsString());
+        currentVote = obj.get("current_vote").getAsString();
+        example = obj.get("example").getAsString();
+        thumbsDown = strToL(obj.get("thumbs_down").getAsString());
+    }
+
+    private long strToL(String l){
+        try{
+            return Long.parseLong(l);
+        }catch (Exception e){
+            return 0;
+        }
+    }
+
+    public String getDefinition(){
+        return def;
+    }
+
+    public String getPermaLink() {
+        return permaLink;
+    }
+
+    public long getThumbsUp() {
+        return thumbsUp;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public long getDefid() {
+        return defid;
+    }
+
+    public String getCurrentVote() {
+        return currentVote;
+    }
+
+    public String getExample() {
+        return example;
+    }
+
+    public long getThumbsDown() {
+        return thumbsDown;
+    }
+
+    @Override
+    public String toString() {
+        return word+"\n\n"+def+"\n\n"+example;
+    }
+}
---
+++ src/com/bittle/urban/UrbanDictionary.java
@@ -0,0 +1,97 @@
+package com.bittle.urban;
+
+import com.bittle.urban.Definition;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UrbanDictionary {
+
+    private final String API_URL = "http://api.urbandictionary.com/v0/define?term=";
+    private JsonObject currentJsonObject = null;
+    private List<Definition> definitions = null;
+
+    private JsonObject read(String url) {
+        try {
+            URL con = new URL(url);
+            HttpURLConnection req = (HttpURLConnection) con.openConnection();
+            req.connect();
+
+            JsonParser parser = new JsonParser();
+            JsonElement root = parser.parse(new InputStreamReader((InputStream) req.getContent()));
+            return root.getAsJsonObject();
+        } catch (Exception e) {
+
+            JsonParser parser = new JsonParser();
+
+            // empty json
+            JsonElement element =
+                    parser.parse("{\"tags\":[],\"result_type\":\"no_results\",\"list\":[],\"sounds\":[]}");
+
+            return element.getAsJsonObject();
+        }
+    }
+
+    // return false if word isn't in dictionary
+    public boolean searchWord(String word) {
+        String fixedWord = (word.trim().replaceAll("\\s+", "+")).toLowerCase();
+        fixedWord = API_URL + fixedWord;
+        currentJsonObject = read(fixedWord);
+        definitions = getDefList();
+
+        return hasDefinition();
+    }
+
+    private List<Definition> getDefList() {
+        if (currentJsonObject != null) {
+            JsonArray array = currentJsonObject.getAsJsonArray("list");
+            List<Definition> list = new ArrayList<>();
+            for (int x = 0; x < array.size(); x++) {
+                JsonObject a = array.get(x).getAsJsonObject();
+                list.add(new Definition(a));
+            }
+
+            return list;
+        } else
+            return null;
+    }
+
+    public List<Definition> getDefinitions() {
+        return definitions;
+    }
+
+    public Definition getDefinition(int index) {
+        if(hasDefinition()) {
+            if (definitions.size() <= index) {
+                index = 0;
+            }
+            return definitions.get(index);
+        }
+        else
+            return null;
+    }
+
+    public Definition getRandomDefinition() {
+
+        if (hasDefinition()) {
+            int a = (int) (Math.random() * (definitions.size() + 1));
+            return definitions.get(a);
+        } else {
+            System.out.println("No Definitions found.\n");
+            return null;
+        }
+    }
+
+    private boolean hasDefinition() {
+        String s = currentJsonObject.get("result_type").getAsString();
+        return !s.equals("no_results");
+    }
+}
